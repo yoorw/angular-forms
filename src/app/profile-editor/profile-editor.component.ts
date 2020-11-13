ba91b0659f842d164a5e1afdda96b7512d8fc5b3
yoorw <yoorw@hotmail.com>
2020-11-13 04:48:38 -0600

    completed FormBuilder and Creating Dynamic Forms using FormArray

--- src/app/profile-editor/profile-editor.component.ts
+++ src/app/profile-editor/profile-editor.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {FormGroup, FormControl, Validators} from '@angular/forms';
+import {FormGroup, FormControl, FormBuilder, FormArray, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-profile-editor',
@@ -7,21 +7,56 @@ import {FormGroup, FormControl, Validators} from '@angular/forms';
   styleUrls: ['./profile-editor.component.css']
 })
 export class ProfileEditorComponent {
-  profileForm = new FormGroup({
-    firstName: new FormControl('', Validators.minLength(4)),
-    lastName: new FormControl(''),
-    address: new FormGroup({
-      street: new FormControl(''),
-      city: new FormControl(''),
-      state: new FormControl(''),
-      zip: new FormControl('')
-    })
+  profileForm = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: [''],
+    address: this.fb.group({
+      street: [''],
+      city: [''],
+      state: [''],
+      zip: ['']
+    }),
+    aliases: this.fb.array([
+      this.fb.control('')
+    ])
   });
+
+  // profileForm = new FormGroup({
+  //   firstName: new FormControl('', Validators.minLength(4)),
+  //   lastName: new FormControl(''),
+  //   address: new FormGroup({
+  //     street: new FormControl(''),
+  //     city: new FormControl(''),
+  //     state: new FormControl(''),
+  //     zip: new FormControl('')
+  //   })
+  // });
+
+  constructor(
+    private fb: FormBuilder
+  ) {}
 
   onSubmit(): void {
     // TODO: Use EventEmitter with form value
     console.log('\n\n THIS IS ONSBUMIT !! \n');
     console.log(this.profileForm.value);
     console.warn(this.profileForm.value);
+  }
+
+  updateProfile() {
+    this.profileForm.patchValue({
+      firstName: 'Nancy',
+      address: {
+        street: '123 Drew Street'
+      }
+    });
+  }
+
+  get aliases() {
+    return this.profileForm.get('aliases') as FormArray;
+  }
+
+  addAlias() {
+    this.aliases.push(this.fb.control(''));
   }
 }
